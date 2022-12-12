1b6c8527a15022613d3d37874cf886c233bee8ae
pnitijarasrat <puriwatds@gmail.com>
2022-12-13 03:26:51 +0700

    add about me and animation note: must remove resume

--- app.js
+++ app.js
@@ -1,5 +1,66 @@
-const downButton = document.querySelector('#arrowDownButton');
+// body selector
+const resumeBody = document.querySelector('.resume-body');
+const homeBody = document.querySelector('.home-body');
+const aboutmeBody = document.querySelector('.aboutme-body')
+// content selector
+const profileContainer = document.querySelector('#profile-container')
+const homeContainer = document.querySelector('#headline')
+const aboutmeContainer = document.querySelector('#aboutme-content')
+// button selector
+const homeDown = document.querySelector('#arrowDownButton');
+const aboutmeUp = document.querySelector('#aboutmeArrowUp')
+const aboutmeDown = document.querySelector('#aboutmeArrowDown')
 
-downButton.addEventListener('click', () => {
 
+function bindTest(el) {
+    el.addEventListener('click', () => { console.log('clicked') })
+}
+
+// What happen in Home Page
+homeContainer.className = 'headliner'
+
+setInterval(() => {
+    homeDown.style.visibility = 'visible'
+}, 1500);
+
+homeDown.addEventListener('click', () => {
+    homeContainer.className = ''
+    aboutmeBody.scrollIntoView();
+    setInterval(() => {
+        aboutmeUp.style.visibility = 'visible'
+        aboutmeDown.style.visibility = 'visible'
+    }, 2000);
+    aboutmeContainer.className = 'loadFromLeft'
 })
+
+// What happen in about me page
+
+aboutmeUp.addEventListener('click', () => {
+    homeBody.scrollIntoView();
+    homeContainer.className = 'headliner'
+    aboutmeUp.style.visibility = 'hidden'
+    aboutmeDown.style.visibility = 'hidden'
+    aboutmeContainer.className = ''
+})
+
+// aboutmeDown.addEventListener('click', () => {
+//     resumeBody.scrollIntoView();
+//     homeContainer.className = 'headliner'
+//     aboutmeUp.style.visibility = 'hidden'
+//     aboutmeDown.style.visibility = 'hidden'
+//     aboutmeContainer.className = ''
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ testapp.js
@@ -0,0 +1,7 @@
+const aboutmeUp = document.querySelector('#aboutmeArrowUp')
+const aboutmeDown = document.querySelector('#aboutmeArrowDown')
+
+setInterval(() => {
+    aboutmeUp.style.visibility = 'visible'
+    aboutmeDown.style.visibility = 'visible'
+}, 1500);
