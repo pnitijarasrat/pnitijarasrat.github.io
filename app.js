56b57159c8f4a032dd4e41f2f2cd68515077755f
pnitijarasrat <puriwatds@gmail.com>
2022-12-13 17:30:30 +0700

    edit aboutme page

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const aboutmeContainer = document.querySelector('#aboutme-content')
 const homeP1 = document.querySelector('.home-p1')
 const homeP2 = document.querySelector('.home-p2')
 const homeIntro = document.querySelector('#intro')
+const p1 = document.querySelector('#p1')
+const p2 = document.querySelector('#p2')
+const p3 = document.querySelector('#p3')
+const p4 = document.querySelector('#p4')
+const p5 = document.querySelector('#p5')
+const p6 = document.querySelector('#p6')
 // button selector
 const homeDown = document.querySelector('#arrowDownButton');
 const aboutmeUp = document.querySelector('#aboutmeArrowUp')
@@ -38,8 +44,31 @@ homeDown.addEventListener('click', () => {
     setInterval(() => {
         aboutmeUp.style.visibility = 'visible'
         aboutmeDown.style.visibility = 'visible'
-    }, 2000);
-    aboutmeContainer.className = 'loadFromLeft'
+    }, 18000);
+    setInterval(() => {
+        p1.style.opacity = 1
+        p1.className = 'aboutmeAnimation'
+    }, 1000);
+    setInterval(() => {
+        p2.style.opacity = 1
+        p2.className = 'aboutmeAnimation'
+    }, 3000);
+    setInterval(() => {
+        p3.style.opacity = 1
+        p3.className = 'aboutmeAnimation'
+    }, 5000);
+    setInterval(() => {
+        p4.style.opacity = 1
+        p4.className = 'aboutmeAnimation'
+    }, 7000);
+    setInterval(() => {
+        p5.style.opacity = 1
+        p5.className = 'aboutmeAnimation'
+    }, 12000);
+    setInterval(() => {
+        p6.style.opacity = 1
+        p6.className = 'aboutmeAnimation'
+    }, 14000);
 })
 
 // What happen in about me page
@@ -52,16 +81,9 @@ aboutmeUp.addEventListener('click', () => {
     }, 4000);
     aboutmeUp.style.visibility = 'hidden'
     aboutmeDown.style.visibility = 'hidden'
-    aboutmeContainer.className = ''
 })
 
-// aboutmeDown.addEventListener('click', () => {
-//     resumeBody.scrollIntoView();
-//     homeContainer.className = 'headliner'
-//     aboutmeUp.style.visibility = 'hidden'
-//     aboutmeDown.style.visibility = 'hidden'
-//     aboutmeContainer.className = ''
-// })
+
 
 
 
--- testapp.js
+++ testapp.js
@@ -1,10 +1,32 @@
-const homeP = document.querySelector('.home-p')
-
-function bindTest(el) {
-    el.addEventListener('click', () => { console.log('clicked') })
-}
+const p1 = document.querySelector('#p1')
+const p2 = document.querySelector('#p2')
+const p3 = document.querySelector('#p3')
+const p4 = document.querySelector('#p4')
+const p5 = document.querySelector('#p5')
+const p6 = document.querySelector('#p6')
 
 setInterval(() => {
-    homeP.style.visibility = 'visible'
-}, 4000);
+    p1.style.opacity = 1
+    p1.className = 'aboutmeAnimation'
+}, 1000);
+setInterval(() => {
+    p2.style.opacity = 1
+    p2.className = 'aboutmeAnimation'
+}, 3000);
+setInterval(() => {
+    p3.style.opacity = 1
+    p3.className = 'aboutmeAnimation'
+}, 5000);
+setInterval(() => {
+    p4.style.opacity = 1
+    p4.className = 'aboutmeAnimation'
+}, 7000);
+setInterval(() => {
+    p5.style.opacity = 1
+    p5.className = 'aboutmeAnimation'
+}, 12000);
+setInterval(() => {
+    p6.style.opacity = 1
+    p6.className = 'aboutmeAnimation'
+}, 14000);
 
